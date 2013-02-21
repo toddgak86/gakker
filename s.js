c60f046ce478e5b7b162f08aa945187bc8875e31
Cooper Reid <cooper@Coopers-iMac.local>
2013-02-21 17:03:28 -0500

    update

--- s.js
+++ s.js
@@ -25,11 +25,13 @@
 				$('head').append('<link rel="shortcut icon" href="/favicon.png" type="image/png">');
 				$('title').text('Yahoo! News');
 				$('#pagelet_side_ads').remove();
+				$('#pagelet_ego_pane_w').remove();
+				$('.fbChatSidebar');
 	        });
 	    }
 	
 	    var getNewCss = function () {
-	        return 'html{color:#000!important;background:#f8f8f8!important}body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,textarea,p,blockquote,th,td,span,a{color:#333!important;background:#f8f8f8!important;background-image:none;border:0;box-shadow:none!important;text-shadow:none!important;font-size:12px; font-weight:300; font-family: \'Arial\';border-color:#ccc!important}a,a:hover,a:link{text-decoration:underline!important;color:#317FBF!important;}';
+	        return 'html{color:#000!important;background:#f8f8f8!important}body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,textarea,p,blockquote,th,td,span,a{color:#333!important;background:#f8f8f8!important;background-image:none;border:0;box-shadow:none!important;text-shadow:none!important;font-size:12px; font-weight:300; font-family: \'Arial\';border-color:#ccc!important}a,a:hover,a:link{text-decoration:underline!important;color:#317FBF!important;}img {opacity:.1;} img:hover {opacity:1;}';
 	    }
 	
 		var init = function () {
